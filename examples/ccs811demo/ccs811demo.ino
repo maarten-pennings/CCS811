d1b971286fd188c33fa1d468aaa489801297baff
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2018-02-06 10:04:09 +0100

    Cleanup output

--- examples/ccs811demo/ccs811demo.ino
+++ examples/ccs811demo/ccs811demo.ino
@@ -8,7 +8,7 @@
 #include "ccs811.h"  // CCS811 library
 
 
-CCS811 ccs811(D3);
+CCS811 ccs811(D3); // nWAKE to D3
 
 
 void print_versions() {
@@ -18,12 +18,12 @@ void print_versions() {
   bool ok= ccs811.versions(&hw_version, &fw_boot_version, &fw_app_version);
   if( ok ) {
     Serial.print("init: CCS811 versions: "); 
-    Serial.print("hw "); Serial.print(hw_version,HEX);
-    Serial.print(", fw_boot "); Serial.print(fw_boot_version,HEX); 
-    Serial.print(", fw_app "); Serial.print(fw_app_version,HEX);
+    Serial.print("hw 0x"); Serial.print(hw_version,HEX);
+    Serial.print(", fw_boot 0x"); Serial.print(fw_boot_version,HEX); 
+    Serial.print(", fw_app 0x"); Serial.print(fw_app_version,HEX);
     Serial.println("");
   } else {
-    Serial.println("init: CCS811 versions: failed");
+    Serial.println("init: CCS811 versions FAILED");
   }
 }
 
@@ -34,18 +34,17 @@ void setup() {
   Serial.println("");
   Serial.println("Starting CCS811 simple demo");
 
-  // Enable I2C for ESP8266 NodeMCU boards [VDD to 3V3, GND to GND, nWAKE to GND, SDA to D2, SCL to D1]
+  // Enable I2C for ESP8266 NodeMCU boards [VDD to 3V3, GND to GND, nWAKE to D3, SDA to D2, SCL to D1]
   Wire.begin(/*SDA*/D2,/*SCL*/D1); 
-  Serial.println("init: I2C up");
   
   // Enable CCS811
   bool ok= ccs811.begin();
-  Serial.println(ok?"init: CCS811 up":"init: CCS811 begin ERROR");
-  // Print versions
+  if( !ok ) Serial.println("init: CCS811 begin FAILED");
   print_versions();
+  
   // Start measuring
   ok= ccs811.start(CCS811_MODE_1SEC);
-  Serial.println(ok?"init: CCS811 started":"init: start ERROR");
+  if( !ok) Serial.println("init: CCS811 start FAILED");
 }
 
 
@@ -62,10 +61,10 @@ void loop() {
   Serial.print("eco2=");    Serial.print(eco2);        Serial.print(" ppm,  ");
   Serial.print("tvoc=");    Serial.print(etvoc);       Serial.print(" ppb,  ");
   Serial.print("errstat="); Serial.print(errstat,HEX); Serial.print("="); Serial.print(ccs811.errstat_str(errstat)); Serial.print( ccs811.errstat_ok(errstat) ? "=VALID&NEW,  " : "=ERROR|OLD,  " );
-  Serial.print("raw=");     Serial.print(raw);         Serial.print(" 6uA/10ADC");
+  Serial.print("raw6=");    Serial.print(raw/1024);    Serial.print(" uA, "); 
+  Serial.print("raw10=");   Serial.print(raw%1024);    Serial.print(" ADC");
   Serial.println();
 
   // Wait
   delay(5000); 
 }
-
